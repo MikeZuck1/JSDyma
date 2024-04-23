8f988999c5b23c8c45b446aef6514bafbef2b44e
Mike Batelahoko <mikebatelahoko96@gmail.com>
2024-04-24 01:10:42 +0200

    I have worked on different type in  JS

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
-// Ici, il s'agit du hoisting. Je peux appeler une fonction avant sa déclaration.
-showTheResult(); // appel à la fonction. 
-function showTheResult() {
-    console.log("I show you the result!");
-};
+const name = 'jean'; // print: tring
+const age = 18; // print number
+const hobbies = null; // print : object
+let action; // print undefined 
+let sym = Symbol(); // print symbol
+let actif = true; // print boolean
+
+// objet litteral
+// print: object
+const ville = {
+    name: "Paris",
+    state: "France"
+}; 
+
+// print: function
+const myFunc = function () { 
+    console.log('hi!');
+} 
+
+// print: object
+const today = new Date();
+const arr = [1, 2, 3];
+const bigNumber = BigInt(12345678901234567890123456789 + 2n)+ 1n;
+
+console.log(typeof bigNumber);
+
+const theFunc = function() {
+    let myNumber = 34;
+    while(myNumber === !34) {
+        return null;
+    }
+}
+theFunc(); // call the function 
