bfaac89b8764d0ef87c3f3f93af5d5b8d29b81bb
Mike Batelahoko <mikebatelahoko96@gmail.com>
2024-05-16 21:52:53 +0200

    Modified

--- src/index.js
+++ src/index.js
@@ -1,25 +1,2 @@
-function Personne(nom, prenom, age, location) {
-    this.nom = nom;
-    this.prenom = prenom;
-    this.age = age;
-    this.location = location;
-}
-
-const sonNom = new Personne("Jack");
-const sonPrenom = new Personne("John"); 
-const leurAge = new Personne(25);
-const leurLocation = new Personne("New York");
-
-console.log(`Le prémon de ${sonNom.nom} est ${sonPrenom.prenom}.`);
-
-let firstLetter = 12;
-let secondLetter = 23;
-let thirdLetter = 5;
-
-console.log(firstLetter ** secondLetter ** thirdLetter); // associativité de droite à gauche. 
-// On peut également utiliser le mot-clé "in" pour vérifier si une propriété existe dans un objet :
-// console.log('nom' in personne); 
-
-// operateur de coalescence (ou opérateur ternaire) :
-let unePersonne = undefined || 0.00; 
-console.log(unePersonne ?? "non defined"); 
+const isAnAstre = "     I'm the sun.    ";
+console.log(isAnAstre.trim());
