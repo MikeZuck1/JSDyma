215c6dca16073824ebfd6aa1963eaf6605775bcc
Mike Batelahoko <mikebatelahoko96@gmail.com>
2024-06-06 15:05:07 +0200

    testing switch case

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
-const obj = {
-    terre: true,
-    lune: false
-};
+console.warn("Attention ! Ce message s'adresse à vous.");
+console.info("Juste petite info!"); 
+console.error("Il y a une petite erreur qui s'est produit");
 
-obj.mars = true;
-console.log(obj); 
+const aide = function() {
+    let isFood = " ";
+
+    switch(isFood) {
+        case 'vitelotte noire':
+            console.log('€0.59 le kilo.');
+            break;
+        case 'kennnebec':
+            console.log('€0.96 le kilo.');
+            break;
+        case 'Yukon':
+            console.log('€0.88 le kilo.');
+            break;
+        case ' ':
+            console.warn('you\'re input is empty or we don\'t have what do you want.');
+        default:
+            console.error('Error!'); 
+    }
+}
+aide(); // result you're input is empty or we don't have what do you want.
